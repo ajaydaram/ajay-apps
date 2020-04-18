071375980ae26c2939bfdcf4238514930469d974
Daram Ajay <ajayforchrist777@gmail.com>
2020-04-18 16:01:47 +0000

    first-commit

---
+++ src/pages/team.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+
+import Layout from "../components/layout"
+
+import SEO from "../components/seo"
+
+const TeamPage = () => (
+  <Layout>
+    <SEO title="Home" />
+    <h1>Our Team</h1>
+    
+  </Layout>
+)
+
+export default  TeamPage
